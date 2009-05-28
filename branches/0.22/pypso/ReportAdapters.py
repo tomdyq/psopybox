5c6444dd143567b0c9daab4183bfd2523adefb4e
caraciol <caraciol@ad59af7c-33a0-11de-82fb-7f665ef830f7>
2009-05-28 04:42:53 +0000

    Changed ReportAdapters.py for support Database Adapter.

--- branches/0.22/pypso/ReportAdapters.py
+++ branches/0.22/pypso/ReportAdapters.py
@@ -17,6 +17,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 
 0.10 2009-05-25 Initial version.
+0.11 2009-05-28 Added support for database adapter (SQLite3 database)
 '''
 
 '''
@@ -30,6 +31,113 @@ limitations under the License.
 
 import csv
 import Consts
+
+
+#DBSQLite Class - Adapter to dump data in SQLite3 database format
+class ReportDB:
+    #The Creator of the ReportDB Class
+    #@param dbname: The database filename
+    #@param identify: the identify if the run
+    #@param resetDB: if True, the database structure will be recreated
+    #@param resetIdentify: if True, the identify with the same name will be overwrite with the new data
+    #@param frequency: the generational dump frequency
+    #@param commit_frequency: the commit frequency
+    def __init__(self,dbname=Consts.CDefDBName,identify=None,resetDB=False,
+                 resetIdentify=True,frequency=Consts.CDefDBStatsGenFreq, 
+                 commit_freq=Consts.CDefDBStatsCommitFreq):
+        
+        if identify is None:
+            self.identify = datetime.datetime.strftime(datetime.datetime.now(),"%d/%m/%y-%H:%M")
+        else:
+            self.identify = identify
+        
+        self.connection = None
+        self.resetDB = resetDB
+        self.resetIdentify = resetIdentify
+        self.dbName = dbname
+        self.typeDict = {types.FloatType: : "real"}
+        self.statsGenFreq = frequency
+        self.cursorPool = None
+        self.commitFreq = commit_freq
+    
+    #The string representation of adapter
+    def __repr__(self):
+        ret = "Report DB Adapter [File='%s', identify='%s']" % (self.dbName, self.identify)
+        return ret
+    
+    #Open the database connection
+    def open(self):
+        print "Opening database, dbname=%s" % self.dbName
+        self.connection = sqlite3.connect(self.dbName)
+        
+        if self.resetDB:
+            self.resetStructure(SwarmStatistics.SwarmStatistics(),TopologyStatistics.TopologyStatistics())
+        
+        if self.resetIdentify:
+            self.resetTableIdentify()
+    
+    #commit changes on database and closes connection
+    def commitAndClose(self):
+        self.commit()
+        self.close()
+    
+    #Close the database connection
+    def close(self):
+        print "Closing the database."
+        if self.cursorPool:
+            self.cursorPool.close()
+            self.cursorPool = None
+        self.connection.close()
+    
+    #Commit changes to database
+    def commit(self):
+        self.connection.commit()
+    
+    #Return a cursor from the pool
+    def getCursor(self):
+        if not self.cursorPool:
+            self.cursorPool = self.connection.cursor()
+            return self.cursorPool
+        else:
+            return self.cursorPool
+        
+    
+    #Create table using the Statistics class structure
+    def createStructure(self,*stats):
+        pass
+    
+    #Delete all records on the table with the same identify
+    def resetTableIdentify(self):
+        c = self.getCursor()
+        stmt = "delete from %s where identify  = ?" % (Consts.CDefReportDBTable)
+        stmt2 = "delete from %s where identify = ?" % (Consts.CDefReportDBTableSwarm)
+        
+        try:
+            c.execute(stmt, (self.identify,))
+            c.execute(stmt, (self.identify,))
+        except sqlite3.OperationalError, expt:
+            if expt.message.find("no such table") >= 0:
+                print "\n ## The DB Adapter can't find the tables ! Consider enable the parameter resetDB ! ##\n"
+        
+        self.commit()
+
+    #Deletes the current structure and creates a new one.
+    def resetStructure(self,*stats):
+        c = self.getCursor()
+        c.execute("drop table if exists %s" % (Consts.CDefReportDBTable,))
+        c.execute("drop table if exists %s" % (Consts.CDefReportDBTableSwarm,))
+        self.commit()
+        self.createStructure(stats)
+
+    #Inserts the statistics into the database
+    #@param stats: The statistics objects
+    #@param topology: The swarm to insert stats (class: Topology.Topology)
+    #@param iteration: The iteration of the insert
+    def insert(self,stats,topology,iteration):
+        pass
+
+
+
 
 #ReportFileCSV Class  - Adapter to dump statistics in CSV format
 class ReportFileCSV:
@@ -60,12 +168,12 @@ class ReportFileCSV:
 
     #Open the CSV file or creates a new file
     def open(self):
-        print "Opening the CSV file to dump statistics [%s]", self.filename
+        print "Opening the CSV file to dump statistics [%s]" % self.filename
         if self.reset: open_mode = "w"
         else: open_mode = "a"
         self.fHandler = open(self.filename,open_mode)
         self.csvWriter = csv.writer(self.fHandler,delimiter=";")
-        
+
     #Closes the CSV file handler"
     def close(self):
         if self.fHandler:
@@ -89,3 +197,4 @@ class ReportFileCSV:
         line.extend(stats[0].asTuple())
         line.extend(stats[1].asTuple())
         self.csvWriter.writerow(line)
+   
